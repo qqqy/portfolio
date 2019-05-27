b74900d6142430442b97993ec4a8178e8d77bd70
qqqy <frogasaurus@hotmail.com>
2019-05-27 11:28:58 -0600

    basic layout of site

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        implementation: require("sass")
+      }
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -26,6 +32,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
