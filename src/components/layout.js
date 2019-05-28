45c3f4890ae894cff2c2495df7afc1d57cfbdf20
qqqy <frogasaurus@hotmail.com>
2019-05-28 13:35:33 -0600

    basic web layout and animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,13 +4,25 @@ import './layout.scss';
 export default (props) => {
   return (
     <>
-      <nav id="menu">
-      <div id="sculpt">
-
+      <div id="menu-animation">
+        <div id="sculpt" />
       </div>
+      <nav id="menu">
+        <div id="welcome">
+          <div id="profile"></div>
+          <h1>Welcome! I'm David.</h1>
+          <h2>What would you like to see?</h2>
+        </div>
+        <ul>
+          <li>Journey</li>
+          <li>Journey</li>
+          <li>Journey</li>
+          <li>Journey</li>
+          <li>Journey</li>
+        </ul>
       </nav>
       <div id="content" >
-      {props.children}
+        {props.children}
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-<h1>HI!</h1>
+
   </Layout>
 )
 
